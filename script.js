1ff234bab5bd7e156ae6f5d9567ab66715acb3f2
Mackenzie Farrell <farrellmr2@gmail.com>
2015-02-19 16:41:07 -0600

    changes to accommodations and Google maps

--- script.js
+++ script.js
@@ -10,16 +10,25 @@ var map = new google.maps.Map(mapCanvas, mapOptions)
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
-
-
-
 function initialize_2() {
 var mapCanvas = document.getElementById('map-canvas-2');
 var mapOptions = {
-  center: new google.maps.LatLng(32.6625, -79.9322),
+  center: new google.maps.LatLng(32.778142, -79.963752),
   zoom: 13,
   mapTypeId: google.maps.MapTypeId.ROADMAP
 }
 var map = new google.maps.Map(mapCanvas, mapOptions)
 }
 google.maps.event.addDomListener(window, 'load', initialize_2);
+
+
+function initialize_3() {
+var mapCanvas = document.getElementById('map-canvas-3');
+var mapOptions = {
+  center: new google.maps.LatLng(32.777826, -79.965783),
+  zoom: 13,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+var map = new google.maps.Map(mapCanvas, mapOptions)
+}
+google.maps.event.addDomListener(window, 'load', initialize_3);
