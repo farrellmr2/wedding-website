a82cdec94d7c6a05a69c22a862efa92505dff4a6
farrellmr2 <Mackenzie@Mackenzies-MacBook-Air.local>
2015-02-18 22:21:28 -0600

    adding images

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 function initialize() {
-        var mapCanvas = document.getElementById('map-canvas');
-        var mapOptions = {
-          center: new google.maps.LatLng(32.6625, -79.9322),
-          zoom: 13,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        var map = new google.maps.Map(mapCanvas, mapOptions)
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+var mapCanvas = document.getElementById('map-canvas');
+var mapOptions = {
+  center: new google.maps.LatLng(32.6625, -79.9322),
+  zoom: 13,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+var map = new google.maps.Map(mapCanvas, mapOptions)
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
+function initialize_2() {
+var mapCanvas = document.getElementById('map-canvas-2');
+var mapOptions = {
+  center: new google.maps.LatLng(32.6625, -79.9322),
+  zoom: 13,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+var map = new google.maps.Map(mapCanvas, mapOptions)
+}
+google.maps.event.addDomListener(window, 'load', initialize_2);
